66ec675c9ef9169ca73e9d828f9247f5479f8d38
Suchindra  Rameswaran <suchindra@Suchindras-MacBook-Pro.local>
2022-08-19 15:19:58 +0800

    theme setup

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,7 +3,18 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
  palette: {
    primary: {
-     main: "#fcba03",
+     main: "#508991",
+     dark: "#172A3A"
    },
+   secondary: {
+    main: "#EAEAEA",
+    light: "#FDFDFF"
+   },
+   error: {
+    main: "#F4E7E1"
+   }, 
+   success: {
+    main: "#508991"
+   }
  },
 });
